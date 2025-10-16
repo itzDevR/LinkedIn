5e3720005f93617085cbfa6924683ec57089632d
Manish Rawat <itzdevraj.rawat@gmail.com>
2025-10-16 18:02:32 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+  )
+}
+
+export default App
