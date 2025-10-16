bb518ba6a1472f539ecbdaa62e700e9bfca25af2
Manish Rawat <itzdevraj.rawat@gmail.com>
2025-10-16 19:17:10 +0530

    singup page is completed backend frontend both

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import userRouter from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 dotenv.config()
 const PORT=process.env.PORT
 const app=express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use('/api/auth',userRouter)
 
 app.listen(PORT,()=>{
